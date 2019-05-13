cc484d652e7a8caab687288c16475f8b88b5666e
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-13 17:36:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,9 @@
 def reverse_each_word (string)
 string_array = Array.new
 reversed_array = Array.new
-letters = Array.new
 string_array = string.split(' ')
-string_array.each do |x|
-counter = 0
-x.length.times do
-  reversed_array << string_array[x].pop
 end
-end
-reversed_array
-end
+
 
 array_form = reverse_each_word("Hello, how are you?")
 puts array_form.inspect
