a33f2c35decba78b25e1f474290ac19a1a2dcf4b
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-13 17:45:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,7 @@ def reverse_each_word (string)
 string_array = Array.new
 reversed_array = Array.new
 string_array = string.split(' ')
-string_array.each do |x|
-  string_array[x] << string_array[x].split('')
+
 end
 string_array
 end
